97abdfd16fff3a14c1f38942fd9755cc0c1e8a2f
Chenpob Nopakiat <jienpowg@gmail.com>
2023-04-02 00:38:22 +0700

    Create delete word function for delete word

--- src/pages/entitles.js
+++ src/pages/entitles.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import { RxCross1 } from "react-icons/Rx";
+import api from '../../service/api';
+import { useRouter } from 'next/router';
 
-export const cancelData = [
-  {
-    id: 1 ,
-    name: "ยกเลิก"
-  },
-  {
-    id: 2 ,
-    name: "cancel"
-  },
-  {
-    id: 3 ,
-    name: "หยุด"
+export async function getServerSideProps(){
+  const res = await fetch("http://localhost:4000/words/")
+  const data = await res.json()
+  // console.log(data)
+  return{
+    props:{words:data}
   }
-]
+}
 
-export default function entitles() {
+export default function Entitles({words}) {
+  
+  const router = useRouter();
+
+  async function deleteWord(word){
+    const res = await api.delete(`words/${word}`)
+    router.push('/entitles')
+    // console.log(res)
+    // console.log(res.data)
+  }
+
   return (
     <>
       <p className='text-3xl ml-5 my-5'>Phrase</p>
@@ -35,21 +41,73 @@ export default function entitles() {
         </div>
         <div className='flex-auto'>
           <ul className='flex flex-wrap'>
-            {cancelData.map((item, index) => {
-              return (
-                <li key={index} className='mx-2 mb-3'>
+            {words.map((item, index) => {
+              if(item.word_type === "cancel"){
+                return (
+                <li key={item._id} className='mx-2 mb-3'>
                   <div className='border-2 px-5 py-2 text-gray-400 rounded hover:bg-gray-200'>
-                   {item.name} 
-                   <button className='ml-2 scale-90 align-middle'><RxCross1/></button>
+                   {item.word_name} 
+                   <button onClick={ e => deleteWord(item._id) }  className='ml-2 scale-90 align-middle'><RxCross1/></button>
                   </div>
                 </li>
               )
+              }
             })}
             <button className='mx-2'>Add word</button>
           </ul>
         </div>
       </div>
       <div className="border-b-2 border-gray-200 m-5" ></div>
+
+      <div className='flex flex-row ml-5 w-full'>
+        <div className='w-1/3'>
+          no
+        </div>
+        <div className='flex-auto'>
+          <ul className='flex flex-wrap'>
+            {words.map((item, index) => {
+              if(item.word_type === "no"){
+                return (
+                <li key={item._id} className='mx-2 mb-3'>
+                  <div className='border-2 px-5 py-2 text-gray-400 rounded hover:bg-gray-200'>
+                   {item.word_name} 
+                   <button className='ml-2 scale-90 align-middle'><RxCross1/></button>
+                  </div>
+                </li>
+              )
+              }
+            })}
+            <button className='mx-2'>Add word</button>
+          </ul>
+        </div>
+      </div>
+      <div className="border-b-2 border-gray-200 m-5" ></div>
+
+
+      <div className='flex flex-row ml-5 w-full'>
+        <div className='w-1/3'>
+          yes
+        </div>
+        <div className='flex-auto'>
+          <ul className='flex flex-wrap'>
+            {words.map((item, index) => {
+              if(item.word_type === "yes"){
+                return (
+                <li key={item._id} className='mx-2 mb-3'>
+                  <div className='border-2 px-5 py-2 text-gray-400 rounded hover:bg-gray-200'>
+                   {item.word_name} 
+                   <button className='ml-2 scale-90 align-middle'><RxCross1/></button>
+                  </div>
+                </li>
+              )
+              }
+            })}
+            <button className='mx-2'>Add word</button>
+          </ul>
+        </div>
+      </div>
+      <div className="border-b-2 border-gray-200 m-5" ></div>
+
     </>
   )
 }
