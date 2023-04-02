604401aee5dd8239197cbfa4c3a69cc15f73c2fc
Chenpob Nopakiat <jienpowg@gmail.com>
2023-04-02 16:03:54 +0700

    Chart js overview

--- src/pages/dataanalytics.js
+++ src/pages/dataanalytics.js
@@ -1,7 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
+import api from '../../service/api'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import { Pie } from 'react-chartjs-2'
 
-export default function dataanalytics() {
+ChartJS.register(ArcElement, Tooltip, Legend, ChartDataLabels);
+
+export async function getServerSideProps() {
+  const res = await api.get('intents')
+  const data = res.data
+  // console.log(data)
+  return {
+    props: { intents: data }
+  }
+}
+
+
+export default function Dataanalytics({intents}) {
+
+  const [intentData, setIntentData] = useState({
+    labels: intents.map((data) => data.intent),
+    datasets: [
+      {
+        labels: "Intent",
+        data: intents.map((data) => data.point),
+        backgroundColor: ['rgba(25,129,89,255)',
+                          'rgba(67,160,105,255)',
+                          'rgba(107,191,119,255)',
+                          'rgba(148,223,133,255)',
+                          'rgba(192,255,146,255)',
+                          'rgba(0,158,143,255)'],
+        borderColor: 'rgba(255,255,255,255)',
+        borderWidth: 1,
+      }
+    ]
+  })
+
+  const [optionValue,setOptionValue] = useState({
+    plugins: {
+      datalabels: {
+        display: true,
+        align: 'bottom',
+        backgroundColor: 'rgba(255,255,255,255)',
+        borderRadius: 3,
+        font: {
+          size: 25,
+        },
+      },
+    },
+  })
+
   return (
-    <div>dataanalytics</div>
+    <>
+      <div>
+        <div>
+          <p className='text-3xl ml-5 my-5'>Overview</p>
+        </div>
+        <div className='flex flex-wrap justify-center'>
+          <div className='w-1/3'>
+            <Pie className='scale-90' data={intentData} option={optionValue} />;
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
